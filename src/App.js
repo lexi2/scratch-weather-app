79b9b9aae25c68a07b4a8f60a8f417e4015061e6
Alex Rosewarne <alex.rosewarne@gmail.com>
2023-01-08 16:43:04 +1100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Scratch Weather App</h1>
+      <div className="Container">
+        <h1>Scratch Weather App</h1>
+
       <footer>
         <p>Open source project created by Alex Rosewarne {" "}
-        <a href='https://github.com/lexi2/scratch-weather-app' target='_blank'>View Source Code on GitHub</a>
+        <a href='https://github.com/lexi2/scratch-weather-app' target='_blank' rel='noreferrer'>View Source Code on GitHub</a>
         </p>
       </footer>
+      </div>
+
     </div>
   );
 }
