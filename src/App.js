4ae059e35debb4f6b1040b54d99db8c7c37e99e0
Alex Rosewarne <alex.rosewarne@gmail.com>
2023-01-08 16:26:17 +1100

    Restart to resolve node errors

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Scratch React Weather App</h1>
-      <p> This project was created by Alex Rosewarne{' '}
-        <a href='https://github.com/lexi2/scratch-weather-app' target='_blank '>View source code on GitHub</a>
-      </p>
+      <h1>Scratch Weather App</h1>
+      <footer>
+        <p>Open source project created by Alex Rosewarne {" "}
+        <a href='https://github.com/lexi2/scratch-weather-app' target='_blank'>View Source Code on GitHub</a>
+        </p>
+      </footer>
     </div>
   );
-};
+}
