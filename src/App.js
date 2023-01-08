c33070e0c4a7dd96370c07129b68264150712783
Alex Rosewarne <alex.rosewarne@gmail.com>
2023-01-08 15:08:03 +1100

    added GitHub URL

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
     <div className="App">
       <h1>Scratch React Weather App</h1>
       <p> This project was created by Alex Rosewarne{' '}
-        <a href='' target='_blank '>View source code on GitHub</a>
+        <a href='https://github.com/lexi2/scratch-weather-app' target='_blank '>View source code on GitHub</a>
       </p>
     </div>
   );
