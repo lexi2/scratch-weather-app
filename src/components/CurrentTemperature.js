58cedd3865b6febb85f6a6e91ae569defbd15042
Alex Rosewarne <alex.rosewarne@gmail.com>
2023-01-15 19:31:49 +1100

    added fahrenheit conversion

--- src/components/CurrentTemperature.js
+++ src/components/CurrentTemperature.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function CurrentTemperature(props) {
-  return (
-    <div className="current-temperature">
-      <strong>{Math.round(props.celsius)}</strong>
-      <span className="units"> °C</span>
-    </div>
-  );
+  const[units, setUnits] = useState("metric");
+  
+  function displayImperial(event){
+    event.preventDefault();
+    setUnits("imperial");
+  }
+
+  function displayMetric(event){
+    event.preventDefault();
+    setUnits("metric");
+    }
+
+  function fahrenheit(){
+    return(props.celsius * 9) / 5 + 32;
+  }
+  
+  if (units === "metric"){
+    return (
+      <div className="current-temperature">
+        <strong>{Math.round(props.celsius)}</strong>
+        <span className="units"> °C | <a href="/" onClick={displayImperial}>°F</a></span>
+      </div>
+    );
+    } else {
+      return (
+        <div className="current-temperature">
+          <strong>{Math.round(fahrenheit())}</strong>
+          <span className="units">  <a href="/" onClick={displayMetric}>°C</a>° | °F</span>
+        </div>
+    );
+    }
 }
