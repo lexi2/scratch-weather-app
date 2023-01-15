7d635aef7a31fa665fd91cae58aafa074e35fa7f
Alex Rosewarne <alex.rosewarne@gmail.com>
2023-01-15 16:33:15 +1100

    started to return some search results.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import './App.css';
-import WeatherOverview from './components/WeatherOverview';
 
 export default function App() {
   return (
     <div className="App">
-      <div className="container container-sm">
-        <h1>Scratch Weather App</h1>
-        <WeatherOverview />
-        <footer>
-          <p>Open source project created by Alex Rosewarne {" "}
-          <a href='https://github.com/lexi2/scratch-weather-app' target='_blank' rel='noreferrer'>View Source Code on GitHub</a>
-          </p>
-        </footer>
-      </div>
-
+      <h1>Scratch React Weather App</h1>
+      <p> This project was created by Alex Rosewarne{' '}
+        <a href='https://github.com/lexi2/scratch-weather-app' target='_blank '>View source code on GitHub</a>
+      </p>
     </div>
   );
-}
+};
