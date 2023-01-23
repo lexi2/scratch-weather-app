de737e7d3f73eb17f32ca73efde86b83d5e18608
Alex Rosewarne <alex.rosewarne@gmail.com>
2023-01-23 16:18:05 +1100

    basic forecast structure and stylings

--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "./WeatherForecast.css"
 
-export default function WeatherForecast(){
+
+export default function WeatherForecast(props){
   return (
-  <p>weekly forecast will be in this spot</p>
+  <div className="WeatherForecast">
+    <div className="row">
+      <div className="col-2 forecast-date">
+      Thu 
+      <img src= {props.data.icon} alt={props.data.description} className="" />
+      <div className="forecast-temps">
+        <span className="forecast-min-temp">10 </span>
+        <span className="forecast-max-temp">20</span>
+      </div>
+      </div>
+    </div>
+  </div>
   )
 }
